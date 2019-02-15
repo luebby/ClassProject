ae2f742566337b2b10ba55ae77a3fb19846481a4
luebby <karsten.luebke@fom.de>
2019-02-15 09:43:50 +0100

    Init + mit Data Literacy.

---
+++ Setup/Pakete-installieren.R
@@ -0,0 +1,5 @@
+# Installiert die nötigen R Pakete
+
+install.packages(c("mosaic", "lsr", "tinytex", "rmarkdown", 
+                   "knitr", "rprojroot", "plotly", "DT", 
+                   "readxl"))
